1ebe45223e1012a1d092bd10f3a6f38c90e9a06f
Kenta Aikawa <kalupas226@gmail.com>
2019-08-18 19:12:23 +0900

    add 2-9-11 & 2-9-12

---
+++ chap2/2-9-11.js
@@ -0,0 +1,22 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        arrival_date: null,
+        min_date: null
+    },
+    created: function () {
+        var dt = new Date();
+        dt.setDate(dt.getDate() + 1);
+        this.arrival_date = this.formatDate(dt);
+        this.min_date = this.arrival_date;
+    },
+    methods: {
+        formatDate: function (dt) {
+            var y = dt.getFullYear();
+            var m = ('00' + (dt.getMonth() + 1)).slice(-2);
+            var d = ('00' + dt.getDate()).slice(-2);
+            var result = y + '-' + m + '-' + d;
+            return result;
+        }
+    }
+})
