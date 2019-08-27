c9f0f6b7694fb2f9ef3544bf9e603090c008c9b5
Kenta Aikawa <kalupas226@gmail.com>
2019-08-27 21:21:19 +0900

    xmlhttprequest add

--- chap4/main.js
+++ chap4/main.js
@@ -1,5 +1,20 @@
 var btnLoad = document.querySelector('#load');
 // 読み込みボタンのクリックイベントハンドラを定義
 btnLoad.addEventListener('click', function(event) {
-    // ここにJSONを読み込む処理を記述する
+    // XMLHttpRequestオブジェクトのインスタンスを生成
+    var xmlHttpRequest = new XMLHttpRequest();
+    // 通信状態の変化を監視するイベントハンドラを設定
+    xmlHttpRequest.onreadystatechange = function() {
+        // レスポンスの受信が正常に完了した時
+        if (this.readyState == 4 && this.status == 200) {
+            // 受信したデータをコンソールに出力する
+            console.log(this.readyState, this.response);
+        }
+    };
+    // レスポンスの形式を指定する
+    xmlHttpRequest.responseType = 'json';
+    // リクエストメソッドと読み込むファイルのパスを指定する
+    xmlHttpRequest.open('GET', 'products.json');
+    // リクエストを送信する（非同期通信を開始する）
+    xmlHttpRequest.send();
 })
