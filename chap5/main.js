80b4e04ac3f942f7b427fdca4f2a0c78230a4345
Kenta Aikawa <kalupas226@gmail.com>
2019-10-05 10:41:15 +0900

    complete chap5

--- chap5/main.js
+++ chap5/main.js
@@ -1,112 +1,105 @@
-var app = document.querySelector('#app');
-
-var taxRate = 0.08;
-
-// イベントハンドラの割り当て
-window.addEventListener('load', onPageLoad, false);
-app.querySelector('#delivery_date').addEventListener('change', onInputChanged, false);
-app.querySelector('#opt1').addEventListener('change', onInputChanged, false);
-app.querySelector('#opt2').addEventListener('change', onInputChanged, false);
-app.querySelector('#opt3').addEventListener('change', onInputChanged, false);
-app.querySelector('#opt4').addEventListener('change', onInputChanged, false);
-
-// イベントハンドラ
-function onPageLoad(event) {
-    var wedding_date = app.querySelector('#wedding_date');
-    var delivery_date = app.querySelector('#delivery_date');
-    var dt = new Date();
-    dt.setMonth(dt.getMonth() + 2);
-    wedding_date.value = formatDate(dt);
-    dt.setDate(dt.getDate() - 7);
-    delivery_date.value = formatDate(dt);
-    delivery_date.setAttribute('min', tommorow());
-    updateForm();
-}
-
-function onInputChanged(event) {
-    updateForm();
-}
-
-function formatDate(dt) {
-    var y = dt.getFullYear();
-    var m = ('00' + (dt.getMonth()+1)).slice(-2);
-    var d = ('00' + dt.getDate()).slice(-2);
-}
-
-function tommorow() {
-    var dt = new Date();
-    dt.setDate(dt.getDate() + 1);
-    return formatDate(dt);
-}
-
-function incTax(untaxed) {
-    return Math.floor(untaxed * (1 + taxRate));
-}
-
-function number_format(val) {
+Vue.filter('number_format', function(val) {
     return val.toLocaleString();
-}
-
-function getDateDiff(dateString1, dateString2) {
-    var date1 = new Date(dateString1);
-    var date2 = new Date(dateString2);
-
-    var msDiff = date1.getTime() - date2.getTime();
-
-    return Math.ceil(msDiff / (1000 * 60 * 60 * 24));
-}
-
-function taxedBasePrice() {
-    var addPrice = 0;
-    var delivery_date = app.querySelector('#delivery_date');
-    var dateDiff = getDateDiff(delivery_date.value, (new Date()).toLocaleString());
-
-    if (21 <= dateDiff && dateDiff < 30) {
-        addPrice = 5000;
-    } else if (14 <= dateDiff && dateDiff < 21) {
-        addPrice = 10000;
-    } else if (7 <= dateDiff && dateDiff < 14) {
-        addPrice = 15000;
-    } else if (3 <= dateDiff && dateDiff < 7) {
-        addPrice = 20000;
-    } else if (dateDiff == 3) {
-        addPrice = 40000;
-    } else if (dateDiff == 2) {
-        addPrice = 45000;
-    } else if (dateDiff == 1) {
-        addPrice = 50000;
+});
+var app = new Vue({
+    el: '#app',
+    data: {
+        taxRate: 0.08,
+        movieType: '余興ムービー',
+        basePrice: 30000,
+        addPrice1: 5000,
+        addPrice2: 10000,
+        addPrice3: 15000,
+        addPrice4: 20000,
+        addPrice5: 40000,
+        addPrice6: 45000,
+        addPrice7: 50000,
+        optPrice: 0,
+        totalPrice: 0,
+        wedding_date: '',
+        delivery_date: '',
+        opt1_use: false,
+        opt1_price: 5000,
+        opt2_use: false,
+        opt2_price: 5000,
+        opt3_use: false,
+        opt3_price: 5000,
+        opt4_num: 0,
+        opt4_price: 500
+    },
+    methods: {
+        incTax: function(untaxed) {
+            return Math.floor(untaxed * (1 + this.taxRate));
+        },
+        getDateDiff: function(dateString1, dateString2) {
+            var date1 = new Date(dateString1);
+            var date2 = new Date(dateString2);
+            var msDiff = date1.getTime() - date2.getTime();
+            return Math.ceil(msDiff / (1000 * 60 * 60 * 24));
+        },
+        formatDate: function(dt) {
+            var y = dt.getFullYear();
+            var m = ('00' + (dt.getMonth()+1)).slice(-2);
+            var d = ('00' + dt.getDate()).slice(-2);
+            return (y + '-' + m + '-' + d);
+        }
+    },
+    computed: {
+        taxedOpt1: function() {
+            return this.incTax(this.opt1_price);
+        },
+        taxedOpt2: function() {
+            return this.incTax(this.opt2_price);
+        },
+        taxedOpt3: function() {
+            return this.incTax(this.opt3_price);
+        },
+        taxedOpt4: function() {
+            return this.incTax(this.opt4_price);
+        },
+        taxedBasePrice: function() {
+            var addPrice = 0;
+            var dateDiff = this.getDateDiff(this.delivery_date, (new Date().toLocaleString()));
+            if (21 <= dateDiff && dateDiff < 30) {
+                addPrice = this.addPrice1;
+            } else if (14 <= dateDiff && dateDiff < 21) {
+                addPrice = this.addPrice2;
+            } else if (7 <= dateDiff && dateDiff <14) {
+                addPrice = this.addPrice3;
+            } else if (3 < dateDiff && dateDiff < 7) {
+                addPrice = this.addPrice4;
+            } else if (dateDiff == 3) {
+                addPrice = this.addPrice5;
+            } else if (dateDiff == 2) {
+                addPrice = this.addPrice6;
+            } else if (dateDiff == 1) {
+                addPrice = this.addPrice7;
+            }
+            return this.incTax(this.basePrice + addPrice);
+        },
+        taxedOptPrice: function() {
+            var optPrice = 0;
+            if (this.opt1_use) { optPrice += this.opt1_price; }
+            if (this.opt2_use) { optPrice += this.opt2_price; }
+            if (this.opt3_use) { optPrice += this.opt3_price; }
+            if (this.opt4_num == '') { this.opt4_num = 0; }
+            optPrice += this.opt4_num * this.opt4_price;
+            return this.incTax(optPrice);
+        },
+        taxedTotalPrice: function() {
+            return (this.taxedBasePrice + this.taxedOptPrice);
+        },
+        tommorow: function() {
+            var dt = new Date();
+            dt.setDate(dt.getDate() + 1);
+            return this.formatDate(dt);
+        }
+    },
+    created: function() {
+        var dt = new Date();
+        dt.setMonth(dt.getMonth() + 2);
+        this.wedding_date = this.formatDate(dt);
+        dt.setDate(dt.getDate() - 7);
+        this.delivery_date = this.formatDate(dt);
     }
-
-    return incTax(30000 + addPrice);
-}
-
-function taxedOptPrice() {
-    var optPrice = 0;
-
-    var opt1 = app.querySelector('#opt1');
-    var opt2 = app.querySelector('#opt2');
-    var opt3 = app.querySelector('#opt3');
-    var opt4 = app.querySelector('#opt4');
-    
-    if (opt1.checked) { optPrice += 5000; }
-    if (opt2.checked) { optPrice += 5000; }
-    if (opt3.checked) { optPrice += 5000; }
-    if (opt4.value == '') { opt4 = 0; }
-    optPrice += opt4.value * 500;
-    
-    return incTax(optPrice);
-}
-
-function updateForm() {
-    var sum_base = app.querySelector('#sum_base');
-    var sum_opt = app.querySelector('#sum_opt');
-    var sum_total = app.querySelector('#sum_total');
-
-    var basePrice = taxedBasePrice();
-    var optPrice = taxedOptPrice();
-    var totalPrice = basePrice + optPrice;
-
-    sum_base.value = number_format(basePrice);
-    sum_opt.value = number_format(optPrice);
-    sum_total.value = number_format(totalPrice);
-}
+});
